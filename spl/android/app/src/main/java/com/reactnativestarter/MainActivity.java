aeb6fc4789e03d2bde9a3d93140829f49766df27
Aaahh Ahh <woohoomoo2u@gmail.com>
2020-02-01 06:30:58 -0500

    android

--- spl/android/app/src/main/java/com/reactnativestarter/MainActivity.java
+++ spl/android/app/src/main/java/com/reactnativestarter/MainActivity.java
@@ -1,8 +1,15 @@
 package com.reactnativestarter;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity{
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import io.radar.sdk.Radar;
+
+public class MainActivity extends ReactActivity {
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -13,32 +20,32 @@ public class MainActivity extends ReactActivity{
         return "ReactNativeStarter";
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        Radar.initialize(this);
-        Radar.setPlacesProvider(RadarPlacesProvider.FACEBOOK);
-        Radar.requestPermissions(this);
-        Radar.startTracking();
-
-    // ...
-    }
-
-    public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
-
-        @Override
-        public void onTokenRefresh() {
-          String fcmToken = FirebaseInstanceId.getInstance().getToken();
-      
-          try {
-            JSONObject metadata = new JSONObject();
-            metadata.put("fcmToken", fcmToken);
-            Radar.setMetadata(metadata);
-          } catch (JSONException e) {
-            // ...
-          }
-        }
-    }
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+////        Radar.initialize(this);
+////        Radar.setPlacesProvider(Radar.RadarPlacesProvider.FACEBOOK);
+////        Radar.requestPermissions(this);
+////        Radar.startTracking();
+//
+//    // ...
+//    }
+//
+//    public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+//
+//        @Override
+//        public void onTokenRefresh() {
+//          String fcmToken = FirebaseInstanceId.getInstance().getToken();
+//
+//          try {
+//            JSONObject metadata = new JSONObject();
+//            metadata.put("fcmToken", fcmToken);
+//            Radar.setMetadata(metadata);
+//          } catch (JSONException e) {
+//            // ...
+//          }
+//        }
+//    }
 
 }
